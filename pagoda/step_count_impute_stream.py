329216aade82fa1b0c896965e079e456ef4147bc
GreysonL <gang_liu@g.harvard.edu>
2019-11-29 18:07:10 -0500

    update step count function

--- pagoda/step_count_impute_stream.py
+++ pagoda/step_count_impute_stream.py
@@ -1,132 +1,149 @@
 import os
 import sys
+import time
 import pickle
-import pandas as pd
 import numpy as np
-from datetime import datetime,timedelta
+import pandas as pd
 from dateutil import tz
-import matplotlib.pyplot as plt
-data_filepath = "/home/gl176/data"
-with open(data_filepath + '/speed1_all.pickle', 'rb') as handle:
-    speed1_all = pickle.load(handle)
-with open(data_filepath + '/speed2_all.pickle', 'rb') as handle:
-    speed2_all = pickle.load(handle)
+from datetime import datetime,timedelta
 
-par1 = 0.95
-par2 = 0.95
-par3 = 0.05
-par4 = 0.30
-par5 = 0.5
-gap = 15
-K = 3
+with open('empirical_steps.pickle', 'rb') as handle:
+  empirical_steps = pickle.load(handle)
 
-def data_stream(patient_id,j):
-  data_folder = data_filepath + "/beiwe_accelerometer/" + patient_id +  "/accelerometer"
-  if os.path.exists(data_folder):
-    data_files = os.listdir(data_folder)
-    dest_path = data_folder + "/" + data_files[j]
-    hour_data = pd.read_csv(dest_path)
-    hour_data["patient"] = patient_id
-    from_zone = tz.gettz('UTC')
-    to_zone = tz.gettz('America/New_York')
-    UTC = [datetime.strptime(i, '%Y-%m-%dT%H:%M:%S.%f') for i in hour_data['UTC time']]
-    EST = [i.replace(tzinfo=from_zone).astimezone(to_zone) for i in UTC]
-    hour_data["date"] = [i.date() for i in EST]
-    hour_data["hour"] = [i.hour for i in EST]
-
-    t_diff = np.array(hour_data['timestamp'][1:hour_data.shape[0]])-np.array(hour_data['timestamp'][0:(hour_data.shape[0]-1)])
-    t_diff = np.insert(t_diff,0,1)
-    index = np.zeros(len(t_diff),dtype=bool)
-    index[t_diff>0] = True
-    processed_data = hour_data.loc[index == True]
-    mag =  np.sqrt(np.array(processed_data["x"])**2+ np.array(processed_data["y"])**2+
-           np.array(processed_data["z"])**2).tolist()
-    processed_data.insert(3, "magnitude", mag, True)
-    del hour_data
-    splitted = dest_path.split("/")
-    gps_dest_path = splitted[0]
-    for i in np.arange(1,len(splitted)):
-      if splitted[i] == "accelerometer":
-        splitted[i] = "gps"
-      gps_dest_path = gps_dest_path + "/" + splitted[i]
-    if os.path.exists(gps_dest_path):
-      hour_gps = pd.read_csv(gps_dest_path)
-      processed_data["latitude"] = np.mean(hour_gps["latitude"])
-      processed_data["longitude"] = np.mean(hour_gps["longitude"])
+## 1. smooth to a certain Hz
+def smooth_data(data,hz):
+  t = datetime.fromtimestamp(data['timestamp'][0]/1000)
+  hr = t.hour
+  t0 = datetime.timestamp(t-timedelta(minutes=t.minute,seconds=t.second,microseconds=t.microsecond))*1000
+  t_seq = np.array(data['timestamp'])
+  ## first locate those active bouts (let min interval be s seconds)
+  num = np.floor((t_seq - t0)/(1/hz*1000))
+  j = 0; i = 1
+  new_data = []
+  while i<(len(t_seq)-1):
+    if num[i]==num[j]:
+      i = i + 1
+      if i == len(t_seq):
+        index = np.arange(j,i)
+        mean_x = np.mean(data['x'][index])
+        mean_y = np.mean(data['y'][index])
+        mean_z = np.mean(data['z'][index])
+        new_data.append([t0+1/hz*1000*num[i],mean_x,mean_y,mean_z])
     else:
-      processed_data["latitude"] = np.nan
-      processed_data["longitude"] = np.nan
-  return processed_data
+      index = np.arange(j,i)
+      mean_x = np.mean(data['x'][index])
+      mean_y = np.mean(data['y'][index])
+      mean_z = np.mean(data['z'][index])
+      new_data.append([t0+1/hz*1000*num[i],mean_x,mean_y,mean_z])
+      j = i
+      i = i+1
+  new_data = np.array(new_data)
+  mag = np.sqrt(new_data[:,1]**2+new_data[:,2]**2+new_data[:,3]**2)
+  stamp = new_data[:,0]
+  return hr,t0,stamp,mag
 
+## 2. minute-wise step estimation funciton
+def step_est_min(stamp,mag,t0,hz,q,c):
+  if np.mean(mag)>8:
+    g = 9.8
+  else:
+    g = 1
+  h = max(np.percentile(mag,q),c*g)
+  output = []
+  for i in range(60):
+    index = (stamp>=t0+i*60*1000)*(stamp<t0+(i+1)*60*1000)
+    sub_mag = mag[index]
+    sub_stamp = stamp[index]
+    if len(sub_mag)<=1:
+      output.append([i,0,0,0])
+    else:
+      step = 0
+      current = min(sub_stamp)-350
+      for j in np.arange(1,len(sub_mag)):
+        if(sub_mag[j]>=h and sub_stamp[j]>=current+350):
+          step = step + 1
+          current = sub_stamp[j]
+      on_time = len(sub_mag)/hz
+      output.append([i,on_time,step,np.floor(step*60/on_time)])
+  output = pd.DataFrame(np.array(output))
+  output.columns = ["min","active_s","step_obs","step_infer"]
+  return output
 
-def step_est(data,m,t_seq,par1,par2,par3,par4):
-  M = np.array(data['magnitude'])
-  h = np.quantile(M,par1)*par2   ## parameter 1
-  g = 9.8
-  t_seq = t_seq-min(t_seq)
-  step = 0
-  time=-350
-  for i in np.arange(1,len(m)):
-    if(m[i]>=h and m[i]-np.quantile(M,par3)>par4*g and t_seq[i]>=time+350):
-      step = step + 1
-      time = t_seq[i]
-  return(step)
+## 3. check if there exists any walk within an interval
+def nearby_walk(data,k,h):
+  walk = (np.array(data['step_infer'])>h)*1
+  record = (np.array(data['active_s'])>0)*1
+  nearby = walk
+  active = record
+  n = data.shape[0]
+  for i in np.arange(1,k+1):
+    nearby = nearby + np.concatenate((walk[np.arange(i,n)],np.zeros(i))) + np.concatenate((np.zeros(i), walk[np.arange(0,n-i)]))
+    active = active + np.concatenate((record[np.arange(i,n)],np.zeros(i))) + np.concatenate((np.zeros(i), record[np.arange(0,n-i)]))
+  final = (np.array(active)>=1)*1
+  final[(active>=1)*(nearby==0)] = 1
+  final[(active>=1)*(nearby>=1)] = 2
+  return final
 
+## 4. impute the steps based on the output of nearby_walk
+def imp_steps(output,final,hr):
+  steps = np.zeros(output.shape[0])
+  for i in range(output.shape[0]):
+    if np.array(output['active_s'])[i]>5:
+      steps[i] = np.array(output['step_infer'])[i]
+    elif final[i] == 0:
+      r = np.random.choice(range(len(empirical_steps['no_records'][hr])),1)
+      steps[i] = empirical_steps['no_records'][hr][r]
+    elif final[i] == 1:
+      r = np.random.choice(range(len(empirical_steps['non_walk'][hr])),1)
+      steps[i] = empirical_steps['non_walk'][hr][r]
+    else:
+      r = np.random.choice(range(len(empirical_steps['walk'][hr])),1)
+      steps[i] = empirical_steps['walk'][hr][r]
+  return(sum(steps))
 
-## count the steps per day
-def hour_step(data,par1,par2,par3,par4,par5):
-  step = 0
-  sensor_on = 0
-  person_on = 0
-  t_seq = np.array(data["timestamp"])
-  t_diff = t_seq[1:len(t_seq)]-t_seq[0:(len(t_seq)-1)]
-  magnitude = np.array(data["magnitude"])
-  start_index = 0
-  for i in range(len(t_diff)):
-    if t_diff[i]>1000*gap or i==len(t_diff)-1:
-      end_index = i
-      if i==len(t_diff)-1:
-        end_index = len(t_diff)
-      sub_mag = magnitude[start_index:(end_index+1)]
-      sub_t= t_seq[start_index:(end_index+1)]
-      sensor_on = sensor_on + (t_seq[end_index] - t_seq[start_index])/60000
-      if max(sub_mag)-min(sub_mag) > par5:
-        person_on = person_on + (t_seq[end_index] - t_seq[start_index])/60000
-        step = step + step_est(data,sub_mag,sub_t,par1,par2,par3,par4)
-      start_index = i+1
-  return([data["patient"][0],data["date"][0],data["hour"][0],data["latitude"][0],data["longitude"][0],
-         sensor_on, person_on, step])
+## 5. put everything together when read in a raw csv file
+def imp_hour(path,hz,q,c,k,h):
+  data = pd.read_csv(path)
+  hr,t0,stamp,mag = smooth_data(data,hz)
+  output = step_est_min(stamp,mag,t0,hz,q,c)
+  final = nearby_walk(output,k,h)
+  step = imp_steps(output,final,hr)
+  return t0,step
 
+## 6. read in all the data from the folder one by one, and fill in the hours without records
+def hourly_step_count(data_path,output_path,hz,q,c,k,h):
+  for i in os.listdir(data_path):
+    result0 = []
+    t_vec = []
+    patient_path = data_path+"/"+i+"/accelerometer/"
+    for j in os.listdir(patient_path):
+      path = patient_path + j
+      t0,step = imp_hour(path,hz,q,c,k,h)
+      t1 = datetime.fromtimestamp(t0/1000)
+      t_vec.append(t0)
+      result0.append([t1.year,t1.month,t1.day,t1.hour,step])
+    t_vec = np.array(t_vec)
+    nrow = int((t_vec[-1]-t_vec[0])/(1000*60*60)+1)
+    result1 = []
+    m = 0
+    for k in range(nrow):
+      current_t = t_vec[0] + 1000*60*60*k
+      if current_t == t_vec[m]:
+        result1.append(result0[m])
+        m = m + 1
+      else:
+        stamp = datetime.fromtimestamp(current_t/1000)
+        r = np.random.choice(range(len(empirical_steps['no_records'][stamp.hour])),60)
+        step = sum(empirical_steps['no_records'][stamp.hour][r])
+        result1.append([stamp.year,stamp.month,stamp.day,stamp.hour,step])
+    result1 = pd.DataFrame(np.array(result1))
+    result1.columns = ["year","month","day","hour","steps"]
+    result1.to_csv(output_path + "/" + i + ".csv", index=False)
 
-def step_impute(step,person_on,sensor_on,hour,K):
-  speed1 = speed1_all[hour]
-  speed2 = speed2_all[hour]
-  r = np.random.choice(range(len(speed1)),K)
-  s1 = speed1[r]
-  s2 = speed2[r]
-  imputed_steps = step + (sensor_on-person_on)*s1 + (60-sensor_on)*s2
-  mean_step = np.mean(imputed_steps)
-  var_step = np.var(imputed_steps)
-  return mean_step,var_step
+## recommended parameters
+hz = 10; q=75; c=1.05; k=60; h=60
 
-
-### save all the step counts tables as csv
-n = len(os.listdir(data_filepath + "/beiwe_accelerometer"))
-for m in range(n):
-  patient_id = os.listdir(data_filepath + "/beiwe_accelerometer")[m]
-  data_folder = data_filepath + "/beiwe_accelerometer/" + patient_id +  "/accelerometer"
-  J = len(os.listdir(data_folder))
-  step_table = pd.DataFrame({"id":[],"date":[],"hour":[],"latitude":[],"longitude":[],"sensor_on":[],
-                             "person_on":[],"step":[],"imp_mean":[],"imp_var":[]})
-  for j in range(J):
-    sys.stdout.write('\r')
-    sys.stdout.write(patient_id + " [%-50s] %d%%" % ('='*int(np.ceil(j/J*50)), np.ceil(j/J*100)))
-    sys.stdout.flush()
-    data =  data_stream(patient_id,j)
-    out =  hour_step(data,par1,par2,par3,par4,par5)
-    imp_mean,imp_var = step_impute(out[7],out[6],out[5],out[2],K)
-    step_table = step_table.append({"id":out[0],"date":out[1],"hour":out[2],"latitude":out[3],"longitude":out[4],
-                     "sensor_on":out[5],"person_on":out[6],"step":out[7],"imp_mean":imp_mean,"imp_var":imp_var},ignore_index=True)
-
-  path = "/home/gl176/results/step_counts/" + str(patient_id)+ ".csv"
-  step_table.to_csv(path,index=False)
+## usage
+data_path = "C:/Users/glius/Downloads/test_data"
+output_path = "C:/Users/glius/Downloads/test_output"
+hourly_step_count(data_path,output_path,hz,q,c,k,h)
