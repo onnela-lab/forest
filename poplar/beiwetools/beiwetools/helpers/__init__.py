f21b712cf484f0110c3f971e12cef39305ad425b
Josh <josh.barback@gmail.com>
2020-07-18 17:35:33 -0400

    templates edit

--- poplar/beiwetools/beiwetools/helpers/__init__.py
+++ poplar/beiwetools/beiwetools/helpers/__init__.py
@@ -2,7 +2,8 @@ from .classes import *
 from .colors import *
 from .decorators import *
 from .functions import *
+from .log import *
 from .process import *
-#from .templates import *
+from .templates import *
 from .time import *
 
--- poplar/beiwetools/beiwetools/helpers/templates.py
+++ poplar/beiwetools/beiwetools/helpers/templates.py
@@ -1,5 +1,4 @@
-'''
-Templates for processing tasks.
+'''Templates for processing tasks.
 '''
 import logging
 
@@ -9,9 +8,9 @@ logger = logging.getLogger(__name__)
 
 class ProcessTemplate():
     '''
-    This class is a template for raw data processing tasks, e.g. summary, calibration, etc.
-    The purpose of this class is to get the do() method, a function that processes lots of files.
-    The inputs for do() might be: 
+    This class is a template for raw data processing tasks, e.g. summary, 
+    calibration, etc.  The purpose of this class is to get the do() method, 
+    a function that processes lots of files.  The inputs for do() might be: 
         - A list of users, 
         - Dictionaries of keyword arguments,
         - Instances of classes such as BeiweConfig, BeiweProject, etc.
@@ -27,20 +26,25 @@ class ProcessTemplate():
     
     Each step above corresponds to a list of functions.  
     - Step (1) probably includes a function to set up output files and folders.
-    - Steps (2a), (2b), (2c) could be captured in a single task, but are split up for legibility.
+    - Step (1) may also include a function to configure logging output.
+    - Steps (2a), (2b), (2c) could be captured in a single task, but are split 
+      up for legibility.
+    - Step (2a) may include functions that:
+        - Create specific files or folders for the user's processed data,
+        - Look up an alternate identifier for the user,
+        - Calculate user-specific parameters.
     - Functions in Step (2b) may implement one or more of the following:
         - A loop through a user's files,
         - A loop through sections of files (e.g. 60-second windows),
         - A loop through multiple files at a time (e.g. all files from a 5-day period).
-    - Functions in (2c) probably write output to files or folders created in step (1) and/or (2a).
-	- Functions in (3) probably write output to files or folders created in step (1).
+    - Functions in (2c) may write to files or folders created in steps (1) and/or (2a).
+	- Functions in (3) may write to files or folders created in step (1).
 
     The do() function manages kwargs while processing tasks. Therefore:
         - Keywords should be used consistently everywhere.
-        - If a function returns something, it should be in the form of a dictionary of keyword arguments.  
+        - If a function returns something, it should be in the form of a 
+        dictionary of keyword arguments.  
         (The beiwetools.helpers.decorators module provides some tools to simplify this.)
-
-    See accrep.do_summary() for an example.
 
     Attributes: 
         name (str): Name or description of process.
@@ -85,7 +89,7 @@ class ProcessTemplate():
         # loop through list of users
         for i in range(n):
             # set up user parameters
-            logger.info('%s: Setting up for user %s of %s...' % (self.name, i, n))           
+            logger.info('%s: Setting up for user %s of %s...' % (self.name, i+1, n))           
             uid = user_ids[i]
             if uid in id_lookup: uid = id_lookup[uid]
             user_kwargs = {'user_id': uid}
@@ -93,17 +97,17 @@ class ProcessTemplate():
             for f in self.setup_user:
                 user_kwargs.update(f.easy(user_kwargs))
             # process user data
-            logger.info('%s: Processing data for user %s of %s...' % (self.name, i, n))                                   
+            logger.info('%s: Processing data for user %s of %s...' % (self.name, i+1, n))                                   
             for f in self.process_user:
                 user_kwargs.update(f.easy(user_kwargs))                
             # update user records
-            logger.info('%s: Writing records for user %s of %s...' % (self.name, i, n))                                   
+            logger.info('%s: Writing records for user %s of %s...' % (self.name, i+1, n))                                   
             for f in self.user_records:
                 user_kwargs.update(f.easy(user_kwargs))                                
         # update process records
         logger.info('%s: Writing process records...' %self.name)                                   
         for f in self.process_records:
-            process_kwargs.update(f.easy(self.kwargs))
+            process_kwargs.update(f.easy(process_kwargs))
         logger.info('%s: Finished.' %self.name)                                   
 
 
