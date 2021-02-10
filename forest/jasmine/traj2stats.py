31577ffb203c1fd388cb04c7841c9da003c65ff3
GreysonL <gang_liu@g.harvard.edu>
2021-02-10 16:41:05 -0500

    Update traj2stats.py

--- forest/jasmine/traj2stats.py
+++ forest/jasmine/traj2stats.py
@@ -116,7 +116,7 @@ def gps_summaries(traj,tz_str,option):
                     sd_f_dur = 0
                 if option=="hourly":
                     if obs_dur==0:
-                        summary_stats.append([year,month,day,hour,np.nan,np.nan,np.nan,np.nan,np.nan,np.nan,np.nan,np.nan,np.nan,np.nan])
+                        summary_stats.append([year,month,day,hour,0,np.nan,np.nan,np.nan,np.nan,np.nan,np.nan,np.nan,np.nan,np.nan])
                     else:
                         summary_stats.append([year,month,day,hour,obs_dur/60,total_pause_time/60,total_flight_time/60,time_at_home/60,
                                   max_dist_home, dist_traveled,av_f_len,sd_f_len,av_f_dur/60,sd_f_dur/60])
@@ -148,7 +148,7 @@ def gps_summaries(traj,tz_str,option):
                         D = pairwise_great_circle_dist(temp[:,[1,2]])
                         diameter = max(D)
                     if obs_dur == 0:
-                        summary_stats.append([year,month,day,np.nan,np.nan,np.nan,np.nan,np.nan,np.nan,np.nan,np.nan,np.nan,np.nan,np.nan,np.nan,np.nan,np.nan,np.nan,np.nan])
+                        summary_stats.append([year,month,day,0,0,0,np.nan,np.nan,np.nan,np.nan,np.nan,np.nan,np.nan,np.nan,np.nan,np.nan,np.nan,np.nan,np.nan])
                     else:
                         summary_stats.append([year,month,day,obs_dur/3600, obs_day/3600, obs_night/3600,total_pause_time/3600,total_flight_time/3600,
                                   time_at_home/3600, max_dist_home/1000, dist_traveled/1000,av_f_len/1000,sd_f_len/1000,av_f_dur/3600,sd_f_dur/3600,
