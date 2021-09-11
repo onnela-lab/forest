10af3924672e97ef8291d2f4a12b3c2737fae1d2
nponarul <nponarul@gmail.com>
2021-09-11 15:53:15 -0400

    Updated main function to use agg_data

--- forest/sycamore/sycamore_main.py
+++ forest/sycamore/sycamore_main.py
@@ -24,7 +24,7 @@ def survey_stats_main(output_dir, study_dir, beiwe_ids, config_path=None, time_s
         Timezone of study. This defaults to 'America/New_York'
     """
 
-    # Read, aggregate and clean data 
+    # Read, aggregate and clean data
     if config_path is None:
         print('No config file provided. Skipping some summary outputs.')
         agg_data = aggregate_surveys_no_config(study_dir, study_tz)
@@ -33,17 +33,16 @@ def survey_stats_main(output_dir, study_dir, beiwe_ids, config_path=None, time_s
         agg_data = aggregate_surveys_config(study_dir, config_path, study_tz)
         # Create changed answers detail and summary
         ca_detail, ca_summary = agg_changed_answers_summary(study_dir, config_path, agg_data, study_tz)
+        ca_detail.to_csv(os.path.join(output_dir, 'answers_data.csv'), index=False)
+        ca_summary.to_csv(os.path.join(output_dir, 'answers_summary.csv'), index=False)
         if time_start is not None and time_end is not None:
             # Create survey submits detail and summary
             ss_detail, ss_summary = survey_submits(study_dir, config_path, time_start, time_end, beiwe_ids, agg_data,
                                                    study_tz)
-            # Write out summaries
-            agg_data.to_csv(os.path.join(output_dir, 'agg_survey_data.csv'), index=False)
-            ca_detail.to_csv(os.path.join(output_dir, 'answers_data.csv'), index=False)
-            ca_summary.to_csv(os.path.join(output_dir, 'answers_summary.csv'), index=False)
             ss_detail.to_csv(os.path.join(output_dir, 'submits_data.csv'), index=False)
             ss_summary.to_csv(os.path.join(output_dir, 'submits_summary.csv'), index=False)
-
+    # Write out summaries
+    agg_data.to_csv(os.path.join(output_dir, 'agg_survey_data.csv'), index=False)
     # Add alternative survey submits table
     submits_tbl = survey_submits_no_config(study_dir, study_tz)
     submits_tbl.to_csv(os.path.join(output_dir, 'submits_alt_summary.csv'), index=False)
