5d79fb6e5af474786c62bf442a8ddaa7ad551a27
Ilya Sytchev <isytchev@hsph.harvard.edu>
2021-09-22 18:02:39 -0400

    Apply PEP8 to setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup, find_namespace_packages
 requires = ['numpy',
             'pandas',
             'scipy',
-            'holidays', # poplar
-            'pytz', # jasmine, poplar
-            'timezonefinder', # poplar
+            'holidays',  # poplar
+            'pytz',  # jasmine, poplar
+            'timezonefinder',  # poplar
             ]
 
 package_data = {'': ['*.csv', '*.json']}
@@ -17,14 +17,14 @@ with open('LICENSE.md') as f:
     license = f.read()
 
 setup(
-    name = 'forest',
-    version = '0.1.1',
-    description = '--add description here--',
-    long_description = readme,
-    author = 'Onnela Lab',
-    author_email = 'onnela.lab@gmail.com',
-    license = license,
-    packages = find_namespace_packages(include = ['forest.*']),
-    package_data = package_data,
-    install_requires = requires
+    name='forest',
+    version='0.1.1',
+    description='--add description here--',
+    long_description=readme,
+    author='Onnela Lab',
+    author_email='onnela.lab@gmail.com',
+    license=license,
+    packages=find_namespace_packages(include=['forest.*']),
+    package_data=package_data,
+    install_requires=requires
 )
