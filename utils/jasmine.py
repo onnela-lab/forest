84953a897da4df6a8c5286d7d36ff15454d2f695
Ilya Sytchev <isytchev@hsph.harvard.edu>
2021-09-22 18:07:26 -0400

    Apply PEP8 to utils

--- utils/jasmine.py
+++ utils/jasmine.py
@@ -21,15 +21,15 @@ summary_data_path = os.path.join(args.summary_data_path, "gps")
 # (assumes that all participants were always in this time zone)
 tz_str = "America/New_York"
 option = "daily"  # generate summary metrics "hourly", "daily" or "both"
-save_traj = False # Save imputed trajectories?
+save_traj = False  # Save imputed trajectories?
 time_start = None
 time_end = None
 beiwe_id = None
 parameters = None
 all_memory_dict = None
-all_BV_set= None
+all_BV_set = None
 
 os.makedirs(summary_data_path, exist_ok=True)
-gps_stats_main(args.raw_data_path, summary_data_path, tz_str, option, save_traj,
-               time_start, time_end, beiwe_id, parameters, all_memory_dict,
-               all_BV_set)
+gps_stats_main(args.raw_data_path, summary_data_path, tz_str, option,
+               save_traj, time_start, time_end, beiwe_id, parameters,
+               all_memory_dict, all_BV_set)
