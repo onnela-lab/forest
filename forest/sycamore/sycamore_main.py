3dc09fd7d28076298a5da4c3c9cab1499a5bb851
Zachary Clement <clementzach@icloud.com>
2022-01-22 10:40:04 -0500

    updated survey_stats_main to pass through the filepath with intervention times

--- forest/sycamore/sycamore_main.py
+++ forest/sycamore/sycamore_main.py
@@ -1,11 +1,11 @@
 import os
 from .functions import aggregate_surveys_config, aggregate_surveys_no_config
-from .survey_config import survey_submits, survey_submits_no_config
+from .survey_config import survey_submits, survey_submits_no_config, get_all_interventions_dict
 from .changed_answers import agg_changed_answers_summary
 
 
 def survey_stats_main(output_dir, study_dir, beiwe_ids, config_path=None, time_start=None, time_end=None,
-                      study_tz=None):
+                      study_tz=None, interventions_filepath = None):
     """
     Args:
     output_dir(str):
@@ -22,6 +22,9 @@ def survey_stats_main(output_dir, study_dir, beiwe_ids, config_path=None, time_s
         List of users in study for which we are generating a survey schedule
     study_tz(str):
         Timezone of study. This defaults to 'America/New_York'
+    interventions_filepath(str):
+        filepath where interventions table is. 
+        NOTE: This should change once we get a data stream with intervention timings in it.
     """
 
     # Read, aggregate and clean data
@@ -37,8 +40,9 @@ def survey_stats_main(output_dir, study_dir, beiwe_ids, config_path=None, time_s
         ca_summary.to_csv(os.path.join(output_dir, 'answers_summary.csv'), index=False)
         if time_start is not None and time_end is not None:
             # Create survey submits detail and summary
+            all_interventions_dict = get_all_interventions_dict(filepath)
             ss_detail, ss_summary = survey_submits(study_dir, config_path, time_start, time_end, beiwe_ids, agg_data,
-                                                   study_tz)
+                                                   study_tz, all_interventions_dict)
             ss_detail.to_csv(os.path.join(output_dir, 'submits_data.csv'), index=False)
             ss_summary.to_csv(os.path.join(output_dir, 'submits_summary.csv'), index=False)
     # Write out summaries
