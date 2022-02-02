81f6bc9df5ea3b673f3ce796717f9dfc01b7bcf4
Ilya Sytchev <isytchev@hsph.harvard.edu>
2022-02-02 15:36:11 -0500

    Type check fix

--- forest/constants.py
+++ forest/constants.py
@@ -4,7 +4,7 @@ import os
 
 # 1.5 seconds delay between each call for ORS API
 # since the API is limited to 40 requests per minute for free accounts
-ORS_API_DELAY = os.getenv("FOREST_ORS_API_DELAY", default=1.5)
+ORS_API_DELAY = float(os.getenv("FOREST_ORS_API_DELAY", default="1.5"))
 # in case of local ORS instance
 ORS_API_BASE_URL = os.getenv("FOREST_ORS_BASE_URL",
                              default="https://api.openrouteservice.org")
