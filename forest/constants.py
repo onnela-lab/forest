d72e0d14fe33862fc622a8375db8d96ea2a7739f
GeorgeEfstathiadis <gioefst2000@gmail.com>
2022-02-02 16:56:15 +0000

    Add constants module for environments vars

---
+++ forest/constants.py
@@ -0,0 +1,13 @@
+"""Contains parameters used for API calls in Forest."""
+
+import os
+
+# 1.5 seconds delay between each call for ORS API
+# since the API is limited to 40 requests per minute for free accounts
+ORS_API_DELAY = os.getenv("FOREST_ORS_API_DELAY", default=1.5)
+# in case of local ORS instance
+ORS_API_BASE_URL = os.getenv("FOREST_ORS_BASE_URL",
+                             default="https://api.openrouteservice.org")
+# in case of local OSM instance
+OSM_OVERPASS_URL = os.getenv("FOREST_OSM_OVERPASS_URL",
+                             default="https://overpass-api.de/api/interpreter")
