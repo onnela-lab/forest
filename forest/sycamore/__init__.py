937761d8a5a00122bee262931f70b1004fcd2ca0
Zachary Clement <clementzach@icloud.com>
2022-03-25 21:46:33 -0400

    allow imports into __init__ so you can do from sycamore import survey_submits

--- forest/sycamore/__init__.py
+++ forest/sycamore/__init__.py
@@ -1,5 +1,5 @@
-from .base import survey_stats_main
-from .submits import survey_submits
-from .submits import survey_submits_no_config
-from .common import aggregate_surveys_config
-from .responses import agg_changed_answers_summary
+from .base import survey_stats_main  # noqa: F401
+from .submits import survey_submits  # noqa: F401
+from .submits import survey_submits_no_config  # noqa: F401
+from .common import aggregate_surveys_config  # noqa: F401
+from .responses import agg_changed_answers_summary  # noqa: F401
