b02bb6a3fbadbea9076be93c82a554a7acbe2805
Zachary Clement <clementzach@icloud.com>
2022-04-08 12:43:32 -0400

    Add forest/mano tutorial

---
+++ tutorials/keyring_studies.py
@@ -0,0 +1,8 @@
+import os
+
+os.environ['BEIWE_URL'] = '' #Insert server name here (e.g. https://studies.beiwe.org)
+os.environ['BEIWE_USERNAME'] = '' #Insert username for logging into the server
+os.environ['BEIWE_PASSWORD'] = '' #Insert password for logging into the server
+os.environ['BEIWE_ACCESS_KEY'] = '' #Insert access key
+os.environ['BEIWE_SECRET_KEY'] = '' #Insert secret key
+
