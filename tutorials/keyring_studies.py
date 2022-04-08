4bd91a2a8ba6b32bf9596907d4dc778e7180c936
Zachary Clement <clementzach@icloud.com>
2022-04-08 12:47:31 -0400

    PEP8 fix

--- tutorials/keyring_studies.py
+++ tutorials/keyring_studies.py
@@ -1,8 +1,17 @@
 import os
 
-os.environ['BEIWE_URL'] = '' #Insert server name here (e.g. https://studies.beiwe.org)
-os.environ['BEIWE_USERNAME'] = '' #Insert username for logging into the server
-os.environ['BEIWE_PASSWORD'] = '' #Insert password for logging into the server
-os.environ['BEIWE_ACCESS_KEY'] = '' #Insert access key
-os.environ['BEIWE_SECRET_KEY'] = '' #Insert secret key
+# Insert server name here (e.g. https://studies.beiwe.org)
+os.environ['BEIWE_URL'] = ''
 
+# Insert username for logging into the server
+os.environ['BEIWE_USERNAME'] = ''
+
+# Insert password for logging into the server
+os.environ['BEIWE_PASSWORD'] = ''
+
+# Insert access key generated under Manage Credentials' under
+# 'Generate New Data-Download API Access Credentials'
+os.environ['BEIWE_ACCESS_KEY'] = ''
+
+# Insert secret key corresponding to above access key
+os.environ['BEIWE_SECRET_KEY'] = ''
