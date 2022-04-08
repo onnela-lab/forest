fd6fc0470a8cc62ae2f0a451841be8466a8585cb
Zachary Clement <clementzach@icloud.com>
2022-04-07 23:33:07 -0400

    Changed responses_by_Submission name in __init__

--- forest/sycamore/__init__.py
+++ forest/sycamore/__init__.py
@@ -1,10 +1,10 @@
 __all__ = ["survey_stats_main", "aggregate_surveys_config",
            "agg_changed_answers_summary", "survey_submits",
-           "survey_submits_no_config", "responses_by_submission"]
+           "survey_submits_no_config", "format_responses_by_submission"]
 
 from .base import compute_survey_stats
 from .common import aggregate_surveys_config
 from .responses import agg_changed_answers_summary
-from .responses import responses_by_submission
+from .responses import format_responses_by_submission
 from .submits import survey_submits
 from .submits import survey_submits_no_config
