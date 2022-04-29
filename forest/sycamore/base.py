4c2c802085d980dbc0767777ce0153f01e385f4d
Zachary Clement <clementzach@icloud.com>
2022-04-28 22:20:26 -0400

    replace time_start/time_end with start_date and end_date

--- forest/sycamore/base.py
+++ forest/sycamore/base.py
@@ -19,7 +19,7 @@ logger = logging.getLogger(__name__)
 def compute_survey_stats(
         study_folder: str, output_folder: str, tz_str: str = "UTC",
         beiwe_ids: Optional[List] = None,
-        time_start: str = EARLIEST_DATE, time_end: str = None,
+        start_date: str = EARLIEST_DATE, end_date: Optional[str] = None,
         config_path: Optional[str] = None,
         interventions_filepath: str = None,
         augment_with_answers: bool = True
@@ -33,9 +33,9 @@ def compute_survey_stats(
         File path to study data
     config_path(str):
         File path to study configuration file
-    time_start(str):
+    start_date(str):
         The earliest date of survey data to read in, in YYYY-MM-DD format
-    time_end(str):
+    end_date(str):
         The latest survey data to read in, in YYYY-MM-DD format
     beiwe_ids(list):
         List of users in study for which we are generating a survey schedule
@@ -53,14 +53,14 @@ def compute_survey_stats(
     os.makedirs(os.path.join(output_folder, "by_survey"), exist_ok=True)
     if beiwe_ids is None:
         beiwe_ids = get_subdirs(study_folder)
-    if time_end is None:
-        time_end = get_month_from_today()
+    if end_date is None:
+        end_date = get_month_from_today()
     # Read, aggregate and clean data
     if config_path is None:
         logger.warning("No config file provided. "
                        "Skipping some summary outputs.")
         agg_data = aggregate_surveys_no_config(
-            study_folder, tz_str, beiwe_ids, time_start, time_end,
+            study_folder, tz_str, beiwe_ids, start_date, end_date,
             augment_with_answers
         )
         if agg_data.shape[0] == 0:
@@ -68,8 +68,8 @@ def compute_survey_stats(
             return True
     else:
         agg_data = aggregate_surveys_config(
-            study_folder, config_path, tz_str, beiwe_ids, time_start,
-            time_end,  augment_with_answers
+            study_folder, config_path, tz_str, beiwe_ids, start_date,
+            end_date,  augment_with_answers
         )
         if agg_data.shape[0] == 0:
             logger.error("Error: No survey data found in %s", study_folder)
@@ -85,10 +85,10 @@ def compute_survey_stats(
             os.path.join(output_folder, "summaries", "answers_summary.csv"),
             index=False
         )
-        if time_start is not None and time_end is not None:
+        if start_date is not None and end_date is not None:
             # Create survey submits detail and summary
             ss_detail, ss_summary = survey_submits(
-                config_path, time_start, time_end,
+                config_path, start_date, end_date,
                 beiwe_ids, agg_data, interventions_filepath
             )
             if ss_summary.shape[0] > 0:
