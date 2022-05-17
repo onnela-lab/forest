92caa1b5f5ac329c4ab799d6f52ae55d03664670
Zachary Clement <clementzach@icloud.com>
2022-05-16 22:02:56 -0400

    remove redundant parentheses; pep8 fixes

--- forest/poplar/functions/helpers.py
+++ forest/poplar/functions/helpers.py
@@ -1,6 +1,6 @@
-'''Tools for common data processing tasks.
+"""Tools for common data processing tasks.
 
-'''
+"""
 import os
 from logging import getLogger
 from collections import OrderedDict
@@ -12,7 +12,7 @@ logger = getLogger(__name__)
 
 def clean_dataframe(df, drop_duplicates = True, sort = True, 
                     update_index = True):
-    '''
+    """
     Clean up a pandas dataframe that contains Beiwe data.
     
     Args:
@@ -24,7 +24,7 @@ def clean_dataframe(df, drop_duplicates = True, sort = True,
             
     Returns:
         None
-    '''
+    """
     if drop_duplicates:
         df.drop_duplicates(inplace = True)
     if sort:
@@ -37,7 +37,7 @@ def clean_dataframe(df, drop_duplicates = True, sort = True,
 
 
 def get_windows(df, start, end, window_length_ms):
-    '''
+    """
     Generate evenly spaced windows over a time period.  For each window, 
     figure out which rows of a data frame were observed during the window.
     Usually df contains an hour of raw sensor data, and start/end are the 
@@ -56,7 +56,7 @@ def get_windows(df, start, end, window_length_ms):
         windows (OrderedDict):  Keys are timestamps corresponding to the 
             beginning of each window.  Values are lists of row indices in df
             that fall within the corresponding window.
-    '''    
+    """
     if (end - start) % window_length_ms != 0:
         logger.warning('The window length doesn\'t \
                        evenly divide the interval.')
@@ -69,13 +69,13 @@ def get_windows(df, start, end, window_length_ms):
                     windows[key] = [i]
                 else:
                     windows[key].append(i)
-            return(windows)
+            return windows
         except:
             logger.warning('Unable to identify windows.')
             
 
 def directory_size(dirpath, ndigits = 1):
-    '''
+    """
     Get the total size in megabytes of all files in a directory (including 
     subdirectories).
     
@@ -86,7 +86,7 @@ def directory_size(dirpath, ndigits = 1):
     Returns:
         size (float): Size in megabytes.
         file_count (int): Number of files in the directory.
-    '''
+    """
     size = 0
     file_count = 0
     for path, dirs, filenames in os.walk(dirpath):
@@ -95,11 +95,11 @@ def directory_size(dirpath, ndigits = 1):
             size += os.path.getsize(filepath)
             file_count += 1
     size = round(size / (2**20), ndigits)    
-    return(size, file_count)
+    return size, file_count
 
 
 def sort_by(list_to_sort, list_to_sort_by):
-    '''
+    """
     Sort a list according to items in a second list.  For example, sort 
     person identifiers according to study entry date.
 
@@ -114,20 +114,20 @@ def sort_by(list_to_sort, list_to_sort_by):
     Example:
         >>> sort_by([1, 2, 3, 4], ['b', 'c', 'a', 'd'])
         [3, 1, 2, 4]        
-    '''
+    """
     if len(list_to_sort) != len(list_to_sort_by):
         logger.warning('Lists are not the same length.')
     else:
         try:
             sorted_list = [i for _, i in \
                            sorted(zip(list_to_sort_by, list_to_sort))]
-            return(sorted_list)
+            return sorted_list
         except:
             logger.warning('Unable to sort list.')               
         
 
 def join_lists(list_of_lists):
-    '''
+    """
     Join all lists in a list of lists.    
 
     Args:
@@ -135,19 +135,19 @@ def join_lists(list_of_lists):
 
     Returns:
         joined_list (list)
-    '''
+    """
     if not all([type(i) is list for i in list_of_lists]):
         logger.warning('This is not a list of lists.')
     else:
         joined_list = [i for sublist in list_of_lists for i in sublist]
-        return(joined_list)    
+        return joined_list
 
 
 # Dictionary of some summary statistics:
-def sample_range(a): return(np.max(a) - np.min(a))
-def iqr(a):          return(np.percentile(a, 75) - np.percentile(a, 25))
-def sample_std(a):   return(np.std(a, ddof = 1))
-def sample_var(a):   return(np.var(a, ddof = 1))
+def sample_range(a): return np.max(a) - np.min(a)
+def iqr(a):          return np.percentile(a, 75) - np.percentile(a, 25)
+def sample_std(a):   return np.std(a, ddof = 1)
+def sample_var(a):   return np.var(a, ddof = 1)
 STATS = {'mean':   np.mean,
          'median': np.median,
          'range':  sample_range,
