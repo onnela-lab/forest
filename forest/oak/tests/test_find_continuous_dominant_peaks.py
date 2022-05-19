fb79f682ff398103382c9d4f4154fb828ccd9493
MStraczkiewicz <mstraczkiewicz@gmail.com>
2022-05-19 13:44:49 -0400

    added tests for 'find_continuous_dominant_peaks'

--- forest/oak/tests/test_find_continuous_dominant_peaks.py
+++ forest/oak/tests/test_find_continuous_dominant_peaks.py
@@ -75,21 +75,21 @@ def expected_output3():
                       [0, 0, 0, 0, 0, 0, 1, 0, 0, 0]]).T)
 
 
-def test_find_continuous_dominant_peaks_params1(test_input, test_output1,
+def test_find_continuous_dominant_peaks_params1(test_input, expected_output1,
                                                 test_params1):
     epsilon, delta = test_params1
     test_output = find_continuous_dominant_peaks(test_input, epsilon, delta)
     assert np.array_equal(test_output, expected_output1)
 
 
-def test_find_continuous_dominant_peaks_params2(test_input, test_output2,
+def test_find_continuous_dominant_peaks_params2(test_input, expected_output2,
                                                 test_params2):
     epsilon, delta = test_params2
     test_output = find_continuous_dominant_peaks(test_input, epsilon, delta)
     assert np.array_equal(test_output, expected_output2)
 
 
-def test_find_continuous_dominant_peaks_params3(test_input, test_output3,
+def test_find_continuous_dominant_peaks_params3(test_input, expected_output3,
                                                 test_params3):
     epsilon, delta = test_params3
     test_output = find_continuous_dominant_peaks(test_input, epsilon, delta)
