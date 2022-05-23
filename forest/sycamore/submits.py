60f6256c231e812fdfb59363e986a0349190bef3
Zachary Clement <clementzach@icloud.com>
2022-05-23 09:37:52 -0400

    added time_to_open

--- forest/sycamore/submits.py
+++ forest/sycamore/submits.py
@@ -291,6 +291,15 @@ def survey_submits(
             agg.beiwe_id == user, 'Local time'
         ].apply(lambda x: 1 if x in opening_times else 0)
 
+    # Get starting times for each survey to allow joining with files in
+    # the by_surveys folder.
+
+    agg["Local time"] = pd.to_datetime(agg["Local time"])
+
+    agg["start_time"] = agg.groupby(["beiwe_id", "survey id","surv_inst_flg"])[
+        "Local time"
+    ].transform("first")
+
     # Merge survey submit lines onto the schedule data and identify submitted
     # lines
     submit_lines = pd.merge(
@@ -299,7 +308,7 @@ def survey_submits(
         ].drop_duplicates(),
         agg[
             ["Local time", "config_id", "survey id", "beiwe_id", "submit_line",
-             "opened_line"]
+             "opened_line", "start_time"]
         ].loc[(agg.submit_line == 1) |
               (agg.opened_line == 1)].drop_duplicates(),
         how="left", left_on=["id", "beiwe_id"],
@@ -331,7 +340,7 @@ def survey_submits(
     # Find whether there were any submissions or openings in each time period
     submit_lines2 = submit_lines.groupby(
         ["delivery_time", "next_delivery_time", "survey id", "beiwe_id",
-         "config_id"]
+         "config_id", "start_time"]
     )[["opened_flg", "submit_flg"]].max().reset_index()
 
     for col in ["delivery_time", "next_delivery_time"]:
@@ -339,7 +348,8 @@ def survey_submits(
 
     # Merge on the times of the survey submission
     merge_cols = ["delivery_time", "next_delivery_time", "survey id",
-                  "beiwe_id", "config_id", "submit_flg", "opened_flg"]
+                  "beiwe_id", "config_id", "submit_flg", "opened_flg",
+                  "start_time"]
     submit_lines3 = pd.merge(
         submit_lines2, submit_lines[merge_cols + ["Local time"]], how="left",
         left_on=merge_cols, right_on=merge_cols
@@ -354,14 +364,15 @@ def survey_submits(
     # Select appropriate columns
     submit_lines3 = submit_lines3[
         ["survey id", "delivery_time", "beiwe_id", "submit_flg", "opened_flg",
-         "submit_time"]
+         "submit_time", "start_time"]
     ]
 
-    submit_lines3["time_to_submit"] = (submit_lines3["submit_time"] -
-                                       submit_lines3["delivery_time"])
-    submit_lines3["time_to_submit"] = [
-        t.seconds for t in submit_lines3["time_to_submit"]
-    ]
+    submit_lines3["time_to_submit"] = (
+            submit_lines3["submit_time"] - submit_lines3["delivery_time"]
+    ).dt.seconds
+    submit_lines3["time_to_open"] = (
+            submit_lines3["start_time"] - submit_lines3["delivery_time"]
+    ).dt.seconds
 
     # Create a summary that has survey_id, beiwe_id, num_surveys, num
     # submitted surveys, average time to submit
@@ -379,18 +390,30 @@ def survey_submits(
         ].groupby(summary_cols)["time_to_submit"].apply(
             lambda x: np.mean(x)
         )
+        avg_time_to_open = submit_lines3.loc[
+            submit_lines3.opened_flg == 1
+        ].groupby(summary_cols)["time_to_open"].apply(
+            lambda x: np.mean(x)
+        )
     else:
         # do the groupby on all rows to avoid getting an error
         avg_time_to_submit = submit_lines3.groupby(summary_cols)[
             "time_to_submit"
         ].apply(lambda x: pd.to_datetime("NaT"))
+        avg_time_to_open = submit_lines3.groupby(summary_cols)[
+            "time_to_open"
+        ].apply(
+            lambda x: np.mean(x)
+        )
 
     submit_lines_summary = pd.concat(
-        [num_surveys, num_complete_surveys, avg_time_to_submit], axis=1
+        [num_surveys, num_complete_surveys, avg_time_to_submit,
+         avg_time_to_open],
+        axis=1
     ).reset_index()
     submit_lines_summary.columns = [
         "survey id", "beiwe_id", "num_surveys", "num_complete_surveys",
-        "avg_time_to_submit"
+        "avg_time_to_submit", "avg_time_to_open"
     ]
 
     # make cols more interpretable as "no survey submitted"
@@ -404,6 +427,12 @@ def survey_submits(
     submit_lines3["time_to_submit"] = np.where(
         submit_lines3["submit_flg"] == 1,
         submit_lines3["time_to_submit"],
+        np.NaN
+    )
+
+    submit_lines3["time_to_open"] = np.where(
+        submit_lines3["opened_flg"] == 1,
+        submit_lines3["time_to_open"],
         np.NaN
     )
 
