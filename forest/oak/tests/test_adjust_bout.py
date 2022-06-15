2e5f7c6c8f4be9c4a0da9b66b3e11f14832daf9b
MStraczkiewicz <mstraczkiewicz@gmail.com>
2022-06-15 17:29:04 -0400

    moved test from test_preprocess_bout and updated one in test_adjust_bout

--- forest/oak/tests/test_adjust_bout.py
+++ forest/oak/tests/test_adjust_bout.py
@@ -9,8 +9,27 @@ def test_adjust_bout(signal_bout, fs):
     t_interp = np.arange(timestamp[0], timestamp[-1], (1/fs))
     f = interpolate.interp1d(timestamp, x)
     x_interp = f(t_interp)
+
+    f = interpolate.interp1d(timestamp, y)
+    y_interp = f(t_interp)
+
+    f = interpolate.interp1d(timestamp, z)
+    z_interp = f(t_interp)
+
     x_interp_adjust = adjust_bout(x_interp, fs)
-    num_seconds_adjust = np.floor(len(x_interp_adjust)/fs)
-    assert len(x_interp_adjust) == 100
-    assert x_interp_adjust[-1] == x_interp[-1]
-    assert int(num_seconds_adjust) == 10
+    y_interp_adjust = adjust_bout(y_interp, fs)
+    z_interp_adjust = adjust_bout(z_interp, fs)
+
+    num_seconds = np.floor(len(x_interp_adjust)/fs)
+
+    x_interp_adjust = x_interp_adjust[:int(num_seconds*fs)]
+    y_interp_adjust = y_interp_adjust[:int(num_seconds*fs)]
+    z_interp_adjust = z_interp_adjust[:int(num_seconds*fs)]
+
+    vm_interp = np.sqrt(x_interp_adjust**2 +
+                        y_interp_adjust**2 +
+                        z_interp_adjust**2) - 1
+
+    assert len(vm_interp) == 100
+    assert np.round(np.mean(vm_interp), 3) == 0.036
+    assert np.round(np.std(vm_interp), 3) == 0.229
