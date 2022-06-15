da29ad510086a479fefe5e67d4d2a282b0e8c453
MStraczkiewicz <mstraczkiewicz@gmail.com>
2022-06-15 13:33:34 -0400

    broke down find_walking into smaller functions, created new tests, updated old ones

--- forest/oak/tests/test_compute_interpolate_cwt.py
+++ forest/oak/tests/test_compute_interpolate_cwt.py
@@ -11,7 +11,7 @@ def test_compute_interpolate_cwt(signal_bout, fs, wavelet):
 
     freqs_interp, coefs_interp = compute_interpolate_cwt(vm_bout, fs, wavelet)
 
-    expected_output_amp = 0.000652
+    expected_output_amp = 0.000482
     expected_output_freqs = np.arange(0.5, 4.5, 0.05)
     assert math.isclose(np.max(coefs_interp), expected_output_amp,
                         abs_tol=1e-4)
