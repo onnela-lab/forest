228f4936b6a6cd4e8a2638c1267df280d084e4e0
Georgios Efstathiadis <gioefst2000@gmail.com>
2023-03-23 17:52:31 -0400

    update forest tutorial with mano, venv and forest updates

---
+++ tutorials/keyring_studies.py
@@ -0,0 +1,9 @@
+import os
+
+os.environ['BEIWE_URL'] = 'https://studies.beiwe.org' #Insert server name here (e.g. https://studies.beiwe.org)
+os.environ['BEIWE_USERNAME'] = '' #Insert username to Beiwe deployment
+os.environ['BEIWE_PASSWORD'] = '' #Insert password to Beiwe Deployment
+os.environ['BEIWE_ACCESS_KEY'] = '' #Insert data-download API access key
+os.environ['BEIWE_SECRET_KEY'] = '' #Insert data-download API secret key
+os.environ['TABLEAU_ACCESS_KEY'] = ''
+os.environ['TABLEAU_SECRET_KEY'] = ''
