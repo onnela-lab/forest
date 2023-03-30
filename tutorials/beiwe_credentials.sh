77b324e59f50802a4d102abbe5a83a0bcbd0b8aa
Georgios Efstathiadis <gioefst2000@gmail.com>
2023-03-30 06:22:25 -0400

    remove shell script for beiwe credentials

--- tutorials/beiwe_credentials.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export BEIWE_URL="https://studies.beiwe.org"  # Insert server name (e.g. https://studies.beiwe.org)
-export BEIWE_USERNAME=""  # Insert username to Beiwe deployment
-export BEIWE_PASSWORD=""  # Insert password to Beiwe Deployment
-export BEIWE_ACCESS_KEY=""  # Insert data-download API access key
-export BEIWE_SECRET_KEY=""  # Insert data-download API secret key
-export TABLEAU_ACCESS_KEY=""
-export TABLEAU_SECRET_KEY=""
