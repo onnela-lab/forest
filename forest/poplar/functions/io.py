e23d6c408dd7cd6c595ad1a89702aa342a157f6e
Georgios Efstathiadis <gioefst2000@gmail.com>
2023-06-08 12:47:45 -0400

    PEP8 style changes

--- forest/poplar/functions/io.py
+++ forest/poplar/functions/io.py
@@ -1,39 +1,38 @@
-'''Functions for input/output tasks.
-
-'''
+"""Functions for input/output tasks."""
 import os
 import json
 from collections import OrderedDict
 from logging import getLogger
 
+from typing import List, Union
+
 
 logger = getLogger(__name__)
 
 
-def setup_directories(dirpath_list):
-    '''
+def setup_directories(dirpath_list: Union[str, List[str]]) -> None:
+    """
     Checks if directories exist; creates them if not.
     Creates intermediate directories if necessary.
 
     Args:
-        dirpath_list (str or list): List of directory paths (str) to create.  
+        dirpath_list (str or list): List of directory paths (str) to create.
             Can also be a single path (str).
 
     Returns:
         None
-    '''
-    if type(dirpath_list) is str:
+    """
+    if isinstance(dirpath_list, str):
         dirpath_list = [dirpath_list]
-    for d in dirpath_list:
-        if not os.path.exists(d):
-            os.makedirs(d)
-        else:
-            logger.warning('Directory already exists.')
+    for directory in dirpath_list:
+        os.makedirs(directory, exist_ok=True)
 
 
-def write_json(dictionary, name, dirpath, indent = 4):
-    '''
-    Writes a dictionary to a JSON file.
+def write_json(
+    dictionary: Union[dict, OrderedDict], name: str,
+    dirpath: str, indent: int = 4
+) -> str:
+    """Writes a dictionary to a JSON file.
 
     Args:
         dictionary (dict or OrderedDict):  Dictionary to write.
@@ -43,18 +42,21 @@ def write_json(dictionary, name, dirpath, indent = 4):
 
     Returns:
         filepath (str): Path to the JSON file.
-    '''
+    """
     try:
-        filepath = os.path.join(dirpath, name + '.json')
-        with open(filepath, 'w') as f:
-            json.dump(dictionary, f, indent = indent)
-        return(filepath)        
-    except:
-        logger.warning('Unable to write JSON file.')
+        filepath = os.path.join(dirpath, name + ".json")
+        with open(filepath, "w") as file:
+            json.dump(dictionary, file, indent=indent)
+        return filepath
+    except Exception:
+        logger.warning("Unable to write JSON file.")
+        return None
 
 
-def read_json(filepath, ordered = False):
-    '''
+def read_json(
+    filepath: str, ordered: bool = False
+) -> Union[dict, OrderedDict]:
+    """
     Read a JSON file into a dictionary.
 
     Args:
@@ -62,22 +64,24 @@ def read_json(filepath, ordered = False):
         ordered (bool):  Returns an ordered dictionary if True.
 
     Returns:
-        dictionary (dict or OrderedDict): The deserialized contents of 
+        dictionary (dict or OrderedDict): The deserialized contents of
             the JSON file.
-    '''
+    """
     try:
-        with open(filepath, 'r') as f:        
+        with open(filepath, "r") as file:
             if ordered:
-                dictionary = json.load(f, object_pairs_hook = OrderedDict)
+                dictionary = json.load(file, object_pairs_hook=OrderedDict)
             else:
-                dictionary = json.load(f)
-        return(dictionary)
-    except:
-        logger.warning('Unable to read JSON file.')
+                dictionary = json.load(file)
+        return dictionary
+    except Exception:
+        logger.warning("Unable to read JSON file.")
 
 
-def setup_csv(name, dirpath, header):
-    '''
+def setup_csv(
+    name: str, dirpath: str, header: List[str]
+) -> str:
+    """
     Creates a csv file with the given column labels.
     Overwrites a file with the same name.
 
@@ -85,43 +89,43 @@ def setup_csv(name, dirpath, header):
         name (str):  Name of csv file to create, without extension.
         dirpath (str):  Path to location for csv file.
         header (list):  List of column headers (str).
-    
+
     Returns:
         filepath (str): Path to the new csv file.
-    '''
-    filepath = os.path.join(dirpath, name + '.csv')
+    """
+    filepath = os.path.join(dirpath, name + ".csv")
     if os.path.exists(filepath):
-        logger.warning('Overwriting existing file with that name.')
-    f = open(filepath, 'w')
-    f.write(','.join(header) + '\n')
-    f.close()
-    return(filepath)    
+        logger.warning("Overwriting existing file with that name.")
+    with open(filepath, "w") as file:
+        file.write(",".join(header) + "\n")
+    return filepath
 
 
-def write_to_csv(filepath, line, missing_strings = ['nan']):
-    '''
+def write_to_csv(
+    filepath: str, line: List[str], missing_strings: List[str] = ["nan"]
+) -> None:
+    """
     Writes line to a csv file.
 
     Args:
         filepath (str): Path to a text file.
-        line (list): Line of items to add to the csv.  Line items are 
+        line (list): Line of items to add to the csv.  Line items are
             converted to strings, joined with ',' and terminated with '\n'.
-        missing_strings (list): List of strings to replace with ''.  Note that 
+        missing_strings (list): List of strings to replace with ''.  Note that
             'nan' covers both float('NaN') and np.nan.
-    
+
     Returns:
         None
-    '''
+    """
     try:
         # Replace None with ''
-        line = ['' if i is None else i for i in line]
+        line = ["" if i is None else i for i in line]
         # Make sure everything is a string
         line = [str(i) for i in line]
         # Replace missing values with ''
-        line = ['' if i in missing_strings else i for i in line]
+        line = ["" if i in missing_strings else i for i in line]
         # Write to file
-        f = open(filepath, "a")
-        f.write(','.join(line) + "\n")
-        f.close()
-    except:
-        logger.warning('Unable to append line to CSV.')
+        with open(filepath, "a") as file:
+            file.write(",".join(line) + "\n")
+    except Exception:
+        logger.warning("Unable to append line to CSV.")
