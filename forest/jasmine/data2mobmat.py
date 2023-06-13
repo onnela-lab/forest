62a513680eab54617e6a4c4b2c528518b478d124
Georgios Efstathiadis <gioefst2000@gmail.com>
2023-06-13 15:30:38 -0400

    revert great_circle_dist args to float

--- forest/jasmine/data2mobmat.py
+++ forest/jasmine/data2mobmat.py
@@ -50,20 +50,20 @@ def cartesian(
 
 
 def great_circle_dist(
-    lat1: Union[float, np.ndarray], lon1: Union[float, np.ndarray],
-    lat2: Union[float, np.ndarray], lon2: Union[float, np.ndarray]
-) -> Union[float, np.ndarray]:
+    lat1: float, lon1: float,
+    lat2: float, lon2: float
+) -> float:
     """This function calculates the great circle distance
         between two locations.
 
     Args:
-        lat1: Union[float, np.ndarray], latitude of location1,
+        lat1: float, latitude of location1,
             range[-180, 180]
-        lon1: Union[float, np.ndarray], longitude of location1,
+        lon1: float, longitude of location1,
             range[-180, 180]
-        lat2: Union[float, np.ndarray], latitude of location2,
+        lat2: float, latitude of location2,
             range[-180, 180]
-        lon2: Union[float, np.ndarray], longitude of location2,
+        lon2: float, longitude of location2,
             range[-180, 180]
     Returns:
         the great circle distance between location1 and location2
