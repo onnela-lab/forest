e7df0ecbafe9af480abcefb6a8af24c52823960c
Georgios Efstathiadis <gioefst2000@gmail.com>
2023-11-14 16:31:44 -0500

    improve readability in tests

--- forest/oak/tests/test_run_hourly.py
+++ forest/oak/tests/test_run_hourly.py
@@ -4,38 +4,38 @@ import numpy as np
 import pandas as pd
 import pytest
 
-from forest.oak.base import run_hourly, find_walking, preprocess_bout
+from forest.oak.base import run_hourly
 from forest.constants import Frequency
 
 
-@pytest.fixture(scope="module")
-def test_input(signal_bout):
-    timestamp, _, x, y, z = signal_bout
+@pytest.fixture
+def sample_run_input(signal_bout):
 
-    t_bout_interp, vm_bout = preprocess_bout(timestamp, x, y, z)
+    t_hours_pd = pd.Series(pd.to_datetime([
+        "2020-02-25 08:00:00-05:00",
+        "2020-02-25 08:00:00-05:00",
+        "2020-02-25 08:00:00-05:00",
+        "2020-02-25 08:00:00-05:00",
+        "2020-02-25 08:00:00-05:00",
+        "2020-02-25 08:00:00-05:00",
+        "2020-02-25 08:00:00-05:00",
+        "2020-02-25 08:00:00-05:00",
+        "2020-02-25 08:00:00-05:00",
+        "2020-02-25 08:00:00-05:00"
+    ], utc=True).tz_convert('US/Eastern'))
 
-    cadence_bout = find_walking(vm_bout)
-
-    t_datetime = [datetime.fromtimestamp(t_ind) for t_ind in t_bout_interp]
-
-    t_series = pd.Series(t_datetime)
-    t_hours_pd = t_series.dt.floor("H")
-
-    from_zone = tz.gettz("UTC")
-    to_zone = tz.gettz("America/New_York")
-
-    # convert t_hours to correct timezone
-    t_hours_pd = t_hours_pd.dt.tz_localize(from_zone).dt.tz_convert(to_zone)
-
-    date_start = datetime(2020, 2, 24, 0, 0, tzinfo=to_zone)
-    date_end = datetime(2020, 2, 25, 0, 0, tzinfo=to_zone)
     days_hourly = pd.date_range(
-        date_start, date_end + timedelta(days=1), freq="h"
-    )[:-1]
+        start='2020-02-24 00:00:00', 
+        end='2020-02-25 23:00:00', 
+        freq='H', 
+        tz='US/Eastern'
+    )
 
-    steps_hourly = np.full((len(days_hourly), 1), np.nan)
-    cadence_hourly = np.full((len(days_hourly), 1), np.nan)
-    walkingtime_hourly = np.full((len(days_hourly), 1), np.nan)
+    cadence_bout = np.array([1.65, 1.6 , 1.55, 1.6 , 1.55, 1.85, 1.8 , 1.75, 1.75, 1.7 ])
+
+    steps_hourly = np.full((48, 1), np.nan)
+    cadence_hourly = np.full((48, 1), np.nan)
+    walkingtime_hourly = np.full((48, 1), np.nan)
 
     return (
         t_hours_pd,
@@ -47,24 +47,24 @@ def test_input(signal_bout):
     )
 
 
-def test_run_hourly_one_hour_data(test_input):
-    run_hourly(*test_input, Frequency.HOURLY)
+def test_run_hourly_one_hour_data(sample_run_input):
+    run_hourly(*sample_run_input, Frequency.HOURLY)
 
-    steps_hourly, cadence_hourly, walkingtime_hourly = test_input[3:]
+    steps_hourly, cadence_hourly, walkingtime_hourly = sample_run_input[3:]
 
     assert len(steps_hourly) - np.sum(np.isnan(steps_hourly)) == 1
     assert len(cadence_hourly) - np.sum(np.isnan(cadence_hourly)) == 1
     assert len(walkingtime_hourly) - np.sum(np.isnan(walkingtime_hourly)) == 1
 
 
-def test_run_hourly_accuracy(test_input):
-    run_hourly(*test_input, Frequency.HOURLY)
+def test_run_hourly_accuracy(sample_run_input):
+    run_hourly(*sample_run_input, Frequency.HOURLY)
 
-    steps_hourly, cadence_hourly, walkingtime_hourly = test_input[3:]
+    steps_hourly, cadence_hourly, walkingtime_hourly = sample_run_input[3:]
 
-    indice = np.where(~np.isnan(steps_hourly))[0]
+    index = np.where(~np.isnan(steps_hourly))[0]
 
     # get non-nan indices
-    assert steps_hourly[indice][0] == 32
-    assert cadence_hourly[indice][0] == 20
-    assert walkingtime_hourly[indice][0] == 1.6
+    assert steps_hourly[index][0] == 32
+    assert cadence_hourly[index][0] == 20
+    assert walkingtime_hourly[index][0] == 1.6
