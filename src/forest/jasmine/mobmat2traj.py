8991aeeee3cfcf15f3b0a4d3c48b40a93b2aaad2
Georgios Efstathiadis <gioefst2000@gmail.com>
2025-01-20 11:31:17 -0500

    style comments

--- src/forest/jasmine/mobmat2traj.py
+++ src/forest/jasmine/mobmat2traj.py
@@ -1253,7 +1253,8 @@ def imp_to_traj(
                 start_idx = end_idx
                 end_idx += 1
 
-                # If the trajectory is multiple pauses, add it to the tidy_trajectory
+                # If the trajectory is multiple pauses,
+                # add it to the tidy_trajectory
                 if temp[start, 0] == 2:
                     tidy_trajectory.append(
                         [
@@ -1262,7 +1263,8 @@ def imp_to_traj(
                             *temp[end, 4:7],
                         ]
                     )
-                # If the trajectory is a flight in one row, add it to the tidy_trajectory
+                # If the trajectory is a flight in one row,
+                # add it to the tidy_trajectory
                 elif end == start:
                     tidy_trajectory.append(
                         [
