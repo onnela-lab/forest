dd5e6309fb08ff596fa98c6a73aa239822f76cd2
Sai Asish Y <say.apm35@gmail.com>
2026-05-24 08:23:03 -0700

    fix(oak): replace deprecated pandas freq aliases 'H'/'T' with 'h'/'min' (#309)
    Signed-off-by: SAY-5 <say.apm35@gmail.com>

--- src/forest/oak/base.py
+++ src/forest/oak/base.py
@@ -673,9 +673,9 @@ def run(study_folder: str, output_folder: str, tz_str: Optional[str] = None,
                 # transform t to full hours
                 t_series = pd.Series(t_datetime)
                 if frequency == Frequency.MINUTE:
-                    t_hours_pd = t_series.dt.floor('T')
+                    t_hours_pd = t_series.dt.floor('min')
                 else:
-                    t_hours_pd = t_series.dt.floor('H')
+                    t_hours_pd = t_series.dt.floor('h')
 
                 # convert t_hours to correct timezone
                 t_hours_pd = t_hours_pd.dt.tz_localize(
