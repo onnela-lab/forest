3e7559c3204598f48478f7dd5b1a4b7118efe88f
Eli Jones <biblicabeebli@gmail.com>
2026-08-25 14:12:51 -0400

    [refactor-oak] updates git blame ignore, fixes deprecation warning

--- src/forest/oak/base.py
+++ src/forest/oak/base.py
@@ -1,11 +1,13 @@
-from forest.oak.analysis import *
-from forest.oak.preprocess import *
-from forest.oak.runners import *
+from forest.oak.analysis import *  # noqa
+from forest.oak.preprocess import *  # noqa
+from forest.oak.runners import *  # noqa
 
 # emit deprecation warning for imports from forest.oak.base
 import warnings
 warnings.warn(
-    "Imports from forest.oak.base are deprecated. "
-    "Please import from forest.oak.analysis, forest.oak.preprocess, or forest.oak.runners directly.",
-    DeprecationWarning,
+    "\nImports from forest.oak.base are deprecated.\n"
+    "Import from `forest.oak.analysis`, `forest.oak.preprocess`, or `forest.oak.runners` instead.\n"
+    "`forest.oak.base` will be removed in a future release of Forest.\n",
+    FutureWarning,
+    stacklevel=2,
 )
